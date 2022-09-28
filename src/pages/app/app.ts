9da7f0ebc0309b5de4fddba2d1ad2e7ffcae369d
dleinwan <dani.l.wander@gmail.com>
2022-09-28 09:51:10 -0400

    added html/css

--- src/pages/app/app.ts
+++ src/pages/app/app.ts
@@ -1,7 +1,11 @@
+import { doc } from 'firebase/firestore';
 import {
+  getInProgressSession,
   getRecentlyCompletedSessionVotes,
+  getUserId,
   sendVote,
 } from '../../common/firebase';
+import { changeCurrColor, findWinner } from '../results/app';
 
 getRecentlyCompletedSessionVotes().then((votes) =>
   console.log('Received votes', votes)
@@ -13,3 +17,78 @@ sendVote({
   userId: 'user-1',
   when: new Date().getTime(),
 });
+
+
+
+// vote buttons 
+const redButton = document.getElementById('red');
+const greenButton = document.getElementById('green');
+const yellowButton = document.getElementById('yellow');
+const blueButton = document.getElementById('blue');
+const orangeButton = document.getElementById('orange');
+
+function handleClick(colorValue: string) {
+
+  getInProgressSession().then((session) => {
+    const userId = getUserId();
+    const sessionId = session.id;
+    const color = colorValue;
+
+    // Disable buttons.
+    sendVote({
+      color,
+      userId,
+      sessionId,
+      when: new Date().getTime(),
+    }).then(() => { }); // Enable buttons);
+
+  });
+}
+
+redButton?.addEventListener("click", () => handleClick('red'));
+greenButton?.addEventListener("click", () => handleClick('green'));
+yellowButton?.addEventListener("click", () => handleClick('yellow'));
+blueButton?.addEventListener("click", () => handleClick('blue'));
+orangeButton?.addEventListener("click", () => handleClick('orange'));
+
+
+
+// timer 
+var intervalID = setInterval(startTimer, 1000);
+const secondsDisplay = document.getElementById('seconds');
+
+var timerLength = 10;
+var currentTime = 10;
+
+//var sessionEnd = false;
+
+function startTimer() {
+  //console.log(currentTime);
+
+  if (currentTime == 0) {
+    currentTime = timerLength + 1;
+    //console.log(currentTime);
+    //sessionEnd = true;
+    setWinner();
+  }
+  //else sessionEnd = false;
+
+  currentTime = currentTime - 1;
+
+  if (secondsDisplay != null) {
+    secondsDisplay.textContent = currentTime.toString();
+  }
+  
+  //console.log('sessionEnd?: ' + sessionEnd);
+}
+
+function setWinner() {
+  var winnerIndex = findWinner();
+  console.log('winner: ' + winnerIndex);
+  changeCurrColor(winnerIndex);
+  console.log('setWinner executed');
+
+}
+
+
+  
--- src/pages/results/app.ts
+++ src/pages/results/app.ts
@@ -1,6 +1,62 @@
+import { doc } from 'firebase/firestore';
 import qrcode from 'qrcode';
+import { getRecentlyCompletedSessionVotes, getVotesForInProgressSession } from '../../common/firebase';
 
 const canvas = document.getElementById('qrcode');
 qrcode.toCanvas(canvas, `${location.origin}/app`, {
-  scale: 8,
+  scale: 5,
 });
+
+// get votes and sort them
+//const votes = getVotesForInProgressSession();
+//console.log('votes: ' + votes);
+
+
+export function findWinner() {
+  // [red, green, yellow, blue, orange]
+  var voteArray = [3, 1, 5, 0, 2];
+  var colorIndex = ['red', 'green', 'yellow', 'blue', 'orange'];
+
+  //console.log(voteArray);
+
+  //voteArray.sort(function(a, b){return b-a});
+
+  var mostVotes = voteArray[0];
+  var mostVotesIndex = 0;
+
+  for (let i = 1; i < 4; i++) {
+    if (voteArray[i] > mostVotes) {
+      mostVotes = voteArray[i];
+      mostVotesIndex = i;
+    }
+  }
+
+  var winner = colorIndex[mostVotesIndex];
+  var winnerIndex = mostVotesIndex;
+  //console.log("winner: " + winner);
+
+  //console.log(voteArray);
+  
+  return winnerIndex;
+}
+
+
+var winnerBox = document.getElementById("currentColor"); 
+
+//if (winnerBox != undefined) {
+  //winnerBox.style.backgroundColor = '#ff0000';
+//}
+
+export function changeCurrColor(winnerIndex) {
+  var hexCodes = ['#ff0000', '#35E529', '#fff000', '#0000ff', '#ff9933'];
+  var winnerHexCode = hexCodes[winnerIndex];
+
+  console.log('winner hex color: ' + winnerHexCode);
+
+  if (winnerBox != undefined) {
+    //winnerBox.style.backgroundColor = winnerHexCode;
+    winnerBox.style.backgroundColor = '#fff000';
+  }
+  console.log('changeCurrColor executed');
+}
+
